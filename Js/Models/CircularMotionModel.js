ec5455cc3897f95c1af948415c5d81a0a32b1682
Brandon Li <brandon.li820@icloud.com>
2020-11-24 20:26:52 -0700

    name change

--- Js/Models/CircularMotionModel.js
+++ Js/Models/CircularMotionModel.js
@@ -1,14 +1,14 @@
 /**
  *
- * Learning App
+ * Coveral
  * CircularMotionModel.js
  *
- * @author Brandon Li <brandon.li820@icloud.com> 
+ * @author Brandon Li <brandon.li820@icloud.com>
  * Created on 3/1/19
  * Copyright © 2019 Brandon Li. All rights reserved.
  *
  * Model for Circular Motion
- * 
+ *
  */
 
  console.log( "Erherh")
